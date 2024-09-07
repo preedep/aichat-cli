c63e615f1d1429cca581c8bd588a9f07ee730d24
Preedee Ponchevin <preedee.digital@gmail.com>
2024-09-07 11:34:35 +0700

    Add chat history tracking and AI responses
    Integrate a history list to track user inputs and AI responses. Enhance spinner style and improve error handling for user input.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
+use colored::Colorize;
+use indicatif::{ProgressBar, ProgressStyle};
 use langchain_rust::chain::{Chain, LLMChainBuilder};
-use langchain_rust::language_models::llm::LLM;
 use langchain_rust::llm::{AzureConfig, OpenAI};
 use langchain_rust::prompt::HumanMessagePromptTemplate;
 use langchain_rust::schemas::Message;
-use langchain_rust::{fmt_message, fmt_template, message_formatter, prompt_args, template_fstring};
+use langchain_rust::{
+    fmt_message, fmt_placeholder, fmt_template, message_formatter, prompt_args, template_fstring,
+};
 use log::{debug, error, info};
 use std::io;
 use std::io::Write;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::time::Duration;
-use colored::Colorize;
-use indicatif::{ProgressBar, ProgressStyle};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -42,23 +43,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     })
     .expect("Error setting Ctrl-C handler");
 
+    let mut history_list = Vec::new();
+
     while running.load(Ordering::SeqCst) {
         // Check if Ctrl-C was pressed before continuing the loop
         if !running.load(Ordering::SeqCst) {
             break;
         }
-        print!("{}", "Please enter some text and press Enter: ".bright_green());
+        print!(
+            "{}",
+            "Please enter some text and press Enter: ".bright_green()
+        );
         // Flush stdout to ensure the message is displayed immediately
         io::stdout().flush().unwrap();
         let mut input = String::new();
-        let result = io::stdin()
-            .read_line(&mut input);
+        let result = io::stdin().read_line(&mut input);
 
         // Check if Ctrl-C was pressed while waiting for input
         if !running.load(Ordering::SeqCst) {
             break;
         }
-        if result.is_err(){
+        if result.is_err() {
             error!("Error reading input: {:?}", result);
             continue;
         }
@@ -68,19 +73,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             break;
         }
 
+        // Add the input to the history list
+        let m = Message::new_human_message(input);
+        history_list.push(m);
+
         // Create a spinner with a custom message
         let spinner = ProgressBar::new_spinner();
         spinner.set_message(format!("{} Asking...", "💡".blue()));
-        spinner.set_style(ProgressStyle::with_template("{spinner:.green} {msg}")
-            .unwrap()
-            .tick_strings(&["|", "/", "-", "\\", "|", "/", "-", "\\"])); // Spinner style
+        spinner.set_style(
+            ProgressStyle::with_template("{spinner:.green} {msg}")
+                .unwrap()
+                .tick_strings(&["|", "/", "-", "\\", "|", "/", "-", "\\"]),
+        ); // Spinner style
         spinner.enable_steady_tick(Duration::from_millis(120));
+
         // We can also guide it's response with a prompt template.
         // Prompt templates are used to convert raw user input to a better input to the LLM.
         let prompt = message_formatter![
             fmt_message!(Message::new_system_message(
                 "You are world class technical documentation writer."
             )),
+            fmt_placeholder!("history"),
             fmt_template!(HumanMessagePromptTemplate::new(template_fstring!(
                 "{input}", "input"
             )))
@@ -92,24 +105,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             .build()
             .unwrap();
 
-        let res =  chain
+        let res = chain
             .invoke(prompt_args! {
             "input" => input,
+                "history" => history_list
                })
             .await;
+
+        // Finish the spinner and clear the message
         spinner.finish_and_clear();
-        match res
-        {
+
+        match res {
             Ok(result) => {
+                // Add the response to the history list
+                let m = Message::new_ai_message(&result);
+                history_list.push(m);
                 println!("{}", result.yellow());
             }
             Err(e) => panic!("Error invoking LLMChain: {:?}", e),
         }
-
-
-
-
-
     }
 
     Ok(())
